3953ac47f15ea991f660aee3d5636e8659085693
oruche <Oruche.chuks@gmail.com>
2022-07-05 16:41:36 -0700

    tas 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the lowercase alphabets
+ */
+
+void print_alphabet_x10(void);
+{
+	int j;
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+	for (j = 'a'; j <= 'z'; k++)
+	}
+_putchar (j);
+{
+	_putchar('\n');
+}
