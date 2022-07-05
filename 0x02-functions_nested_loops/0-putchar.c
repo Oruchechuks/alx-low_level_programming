c2e0bc22990a5514a0dab17e3ae3c0ed034a310a
oruche <Oruche.chuks@gmail.com>
2022-07-05 14:49:02 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,16 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('p') ;
+	_putchar('u') ;
+	_putchar('t') ;
+	_putchar('c') ;
+	_putchar('h') ;
+	_putchar('a') ;
+	_putchar('r') ;
+	_putchar('\n') ;
+
+
+	return (0)
+
 }
-
-return (o)
-
