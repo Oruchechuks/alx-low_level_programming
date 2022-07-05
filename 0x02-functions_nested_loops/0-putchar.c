41b6d96c5b5886e40b9d79bb144985ff0b74d680
oruche <Oruche.chuks@gmail.com>
2022-07-05 13:31:56 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print _putchar follwed by a new line 
+ *
+ * Return: always 0
+ * /
+
+int main(void)
+
+{
+	printf("_putchar\n");
+}
+
+return (0);
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Contains functions, nested loop task
