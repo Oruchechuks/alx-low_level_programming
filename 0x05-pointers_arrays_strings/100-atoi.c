2868ece724ce7090c75973a7be33e74b4e242551
oruche <Oruche.chuks@gmail.com>
2022-07-12 19:00:52 -0700

    tasks

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: the string to be converted
+ * Return: The integer value of the converted good
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = o;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
+}
