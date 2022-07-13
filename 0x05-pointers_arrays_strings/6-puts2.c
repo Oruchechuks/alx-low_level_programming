7fa2df2f52d218f67f4f2ffb695b35d5236e3b2a
oruche <Oruche.chuks@gmail.com>
2022-07-12 19:25:48 -0700

    tasks

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,7 +4,7 @@
  * puts2 - Prints one char out of two of a string.
  * @str: The string containing characters.
  */
-void puts(char *str)
+void puts2(char *str)
 {
 	int i = 0, len = 0;
 
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _strcpy - Copies of a string pointed to by @src, including the 
+ * _strcpy - Copies of a string pointed to by @src, including the
  * terminating null byte, to a buffer pointed to by @dest.
  * @dest:A buffer to copy the string to.
  * @src: A source string to copy.
  *
  * Return: Apointer to the direction string @dest.
  */
-char *_strcpy (char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
