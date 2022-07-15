bd822befd916caa9e86863063f15b272a50f8bc2
oruche <Oruche.chuks@gmail.com>
2022-07-14 20:57:58 -0700

    tasks

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 
 int main(void)
-
 {
+	int n;
+	int a[5];
+	int *p;
+	
+	a[2] = 1024;
+	p = &n;
 
-  int n;
-
-  int a[5];
-
-  int *p;
-
-
-
-  a[2] = 1024;
-
-  p = &n;
-
-/**
+/*
  * write your line of code here...
  * Remember:
  * - you are not allowed to use a
@@ -33,5 +26,4 @@ int main(void)
   printf("a[2] = %d\n", a[2]);
 
   return (0);
-
 }
