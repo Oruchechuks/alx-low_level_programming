42dfbf0d612bedca51aca3a4230aed939a56e004
oruche <oruche.chuks@gmail.com>
2022-08-01 14:47:13 -0700

    tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print name of file copied from followed
+ * by a new line
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
